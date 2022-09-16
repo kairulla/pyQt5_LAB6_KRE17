5d85b13f2a5b5b16c993d580b10fdb66700c8002
kairulla <kayrulla@bk.ru>
2022-09-17 01:00:12 +0600

    нарисовал яблоко

---
+++ MyApplePicture.py
@@ -0,0 +1,84 @@
+import turtle
+
+turtle.speed(200)
+turtle.fillcolor("#E50000")
+turtle.begin_fill()
+
+sideAngle = 180  # в градусах 100, два бока
+topAngle = -180  # в градусах 100, две верхушки
+topSideAngle = 90  # в градусах 90, четыре штуки
+
+topRadius = 10
+topSideRadius = 20
+sideRadius = 100
+
+turtle.circle(sideRadius, sideAngle)
+turtle.circle(topSideRadius, topSideAngle)
+
+turtle.left(180)
+turtle.circle(topRadius, topAngle)  # середина верхушки, зелёный лист
+## print(turtle.position())
+turtle.left(180)
+turtle.circle(topSideRadius, topSideAngle)
+turtle.circle(sideRadius, sideAngle)
+
+turtle.circle(topSideRadius, topSideAngle)
+turtle.left(180)
+turtle.circle(topRadius, topAngle)
+
+turtle.left(180)
+turtle.circle(topSideRadius, topSideAngle)
+
+turtle.end_fill()
+
+###################################### листок
+
+turtle.fillcolor("#32CD32")
+turtle.begin_fill()
+
+turtle.up()
+turtle.goto(-30.00, 170.00)
+turtle.down()
+
+turtle.left(270)  # 270
+turtle.circle(102, -40)  # правая сторона
+
+turtle.left(320) # 320
+turtle.circle(40, 120)  # 40 120 левая сторона
+
+turtle.end_fill()
+
+###################################### тёмно-красное пятно справа
+## turtle.speed(10)
+turtle.fillcolor("#FF0000")
+turtle.begin_fill()
+
+turtle.up()
+turtle.goto(20.00, 40.00) # 20 40
+
+turtle.left(400)  # 360
+turtle.circle(40, 120)  # 40 120 левая сторона
+
+turtle.left(320) # 320
+turtle.circle(102, -40)  # правая сторона
+
+turtle.end_fill()
+
+###################################### белое пятно слева
+## turtle.speed(10)
+turtle.fillcolor("#FFFFFF")
+turtle.begin_fill()
+
+turtle.goto(-120.00, 100.00)
+
+turtle.left(230)  # 270
+turtle.circle(102, -40)  # правая сторона
+
+turtle.left(320) # 320
+turtle.circle(40, 120)  # 40 120 левая сторона
+
+turtle.end_fill()
+
+
+
+turtle.done()
---
+++ README.md
@@ -0,0 +1,3 @@
+<img src="_MY_PICTURES/Apple.png" width=50% align="middle"><img src="_MY_PICTURES/2022-09-16_235958.png" width=50% align="middle">
+<img src="_MY_PICTURES/Maket.png" width=50% align="middle"><img src="_MY_PICTURES/TASK_6.png" width=50% align="top">
+<img src="_MY_PICTURES/2022-09-16_235908.png" width=50% align="middle">
